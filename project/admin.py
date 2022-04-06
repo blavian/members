ca419efd97e641bf402d565c6031b8c498ee22d2
blavian <benylavian@gmail.com>
2022-04-05 20:54:30 -0400

    registered models with django admin

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Account, Members
 
-# Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+
+admin.site.register(Account,AccountAdmin)
+
+class MembersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone_number', 'client_member_id','account')
+    search_fields = ('first_name', 'last_name', 'phone_number')
+
+admin.site.register(Members, MembersAdmin)
