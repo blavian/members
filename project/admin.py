8841853c4cb84ca236ebabb323bc4c039f0dbc5d
blavian <benylavian@gmail.com>
2022-04-10 11:49:08 -0400

    function for the url

--- project/admin.py
+++ project/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.urls import path
 from .models import Account, Members
 
 class AccountAdmin(admin.ModelAdmin):
@@ -9,5 +10,10 @@ admin.site.register(Account,AccountAdmin)
 class MembersAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'phone_number', 'client_member_id','account')
     search_fields = ('first_name', 'last_name', 'phone_number')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path('upload-csv/', self.upload_csv),]
+        return new_urls + urls
 
 admin.site.register(Members, MembersAdmin)
