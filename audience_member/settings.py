fcb98541e71b716c71f609f6c9340e6ecd2c889c
blavian <benylavian@gmail.com>
2022-04-10 11:35:15 -0400

    settings.py added dir

--- audience_member/settings.py
+++ audience_member/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'audience_member.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
