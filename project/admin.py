838d915636026d55d09422d83c791e6d07e4216a
blavian <benylavian@gmail.com>
2022-04-10 14:28:08 -0400

    import export

--- project/admin.py
+++ project/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
+from import_export import resources
 from .models import Account, Members
+from import_export.admin import ImportExportModelAdmin
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('id','name')
 
 admin.site.register(Account,AccountAdmin)
 
-class MembersAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone_number', 'client_member_id','account')
-    search_fields = ('first_name', 'last_name', 'phone_number')
+    
+class MemberResource(resources.ModelResource):
+    class Meta:
+        model = Members
+        fields = ('id', 'first_name', 'last_name', 'phone_number', 'client_member_id','account')
 
+class MembersAdmin(ImportExportModelAdmin):
+    resource_class = MemberResource
+    list_display = ('id', 'first_name', 'last_name', 'phone_number', 'client_member_id','account')
+    search_fields = ('id','first_name', 'last_name', 'phone_number')
 admin.site.register(Members, MembersAdmin)
