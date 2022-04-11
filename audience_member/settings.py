1ec5477ce4a562f0d62a6344c26b01d7b34763ea
blavian <benylavian@gmail.com>
2022-04-11 08:41:13 -0400

    added settings.py for celery

--- audience_member/settings.py
+++ audience_member/settings.py
@@ -132,6 +132,16 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
+
+
 IMPORT_EXPORT_CELERY_MODELS = {
     "Audience_Member": {"app_label": "audience_member", "model_name": "Members"}
 }
