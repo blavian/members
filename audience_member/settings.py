f90c6823cc12750d35e40496be3358e237801762
blavian <benylavian@gmail.com>
2022-04-10 21:05:54 -0400

    changed pagination to 25

--- audience_member/settings.py
+++ audience_member/settings.py
@@ -132,5 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 4
+    'PAGE_SIZE': 25
     }
